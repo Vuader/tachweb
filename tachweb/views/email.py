7678aa5a32de4060716466f2346deaa3b4736272
Vuader <dave@tachyonic.co.za>
2018-06-05 11:27:03 +0200

    Finished with newsletters

--- tachweb/views/email.py
+++ tachweb/views/email.py
@@ -1,8 +1,14 @@
+import email
+
 from luxon import register
 from luxon import render_template
 from luxon.constants import TEXT_HTML
 from luxon import GetLogger
 from luxon import g
+from luxon import sendmail
+from luxon import db
+from luxon.utils.mail import format_msg
+from datetime import datetime
 
 log = GetLogger(__name__)
 
@@ -13,5 +19,59 @@ g.nav_menu.add('/Admin/News Letter',
 @register.resource('GET', '/newsletter-admin', tag='newsletter_admin')
 def newsletter(req, resp):
     resp.content_type = TEXT_HTML
+    with db() as conn:
+        r = conn.execute('SELECT * FROM newsletters')
     return render_template('tachweb/newsletter_admin.html',
-                           title="Newsletter Admin")
+                           title="Newsletter Admin",
+                           newsletters=r)
+
+
+@register.resource('GET', '/send/{id}', tag='newsletter_admin')
+def send_newsletter(req, resp, id):
+    with db() as conn:
+        result = conn.execute('SELECT message FROM newsletters WHERE id=?',
+                              (id,))
+    msg = result['message']
+    msg = email.message_from_string(msg)
+    year = datetime.today().strftime("%Y")
+    month = datetime.today().strftime("%B")
+    html_template = render_template('tachweb/email.html',
+                                    subject=msg['Subject'],
+                                    year=year,
+                                    month=month)
+    text_template = render_template('tachweb/email.txt',
+                                    subject=msg['Subject'],
+                                    year=year,
+                                    month=month)
+    with db() as conn:
+        results = conn.execute('SELECT * FROM newslist')
+        send_succeeded = False
+        for rcpt in results:
+            to = "%s <%s>" % (rcpt['name'], rcpt['email'],)
+            new = format_msg(msg,
+                             html_template=html_template,
+                             text_template=text_template,
+                             email_from='no-reply@tachyonic.org',
+                             email_to=to,
+                             multipart=True,
+                             id=rcpt['id'],
+                             name=rcpt['name'])
+            try:
+                sendmail('no-reply@tachyonic.org', to, msg=new)
+                send_succeeded = True
+            except Exception as e:
+                log.critical('Failed to send to %s (%s)' % (rcpt, e,))
+
+        if send_succeeded:
+            sql = 'UPDATE newsletters SET last_sent=? WHERE id=?'
+            conn.execute(sql,(datetime.now(),id))
+
+    return newsletter(req,resp)
+
+
+@register.resource('GET', '/view/{id}', tag='newsletter_admin')
+def preview_newsletter(req, resp, id):
+    resp.content_type = TEXT_HTML
+    with db() as conn:
+        r = conn.execute('SELECT message FROM newsletters WHERE id=?',(id,))
+        return r['message']
